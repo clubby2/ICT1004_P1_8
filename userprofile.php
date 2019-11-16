c5e16c5b48ab3394fc5d72d1636ed22037247b78
hengsf <56445931+hengsf@users.noreply.github.com>
2019-11-16 17:57:45 +0800

    Modified userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -5,7 +5,7 @@
         <title>Product</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/index.css" rel="stylesheet" >
-                <link href="css/profile.css" rel="stylesheet" >
+        <link href="css/profile.css" rel="stylesheet" >
         <link href="css/bootstrap.min.css" rel="stylesheet" >
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
         <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
@@ -21,15 +21,17 @@
         <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script scr="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         <script defer src="js/index.js"></script>
-
+        <script defer src="js/pymtshipping.js"></script>
         <script defer src="js/headnfoot.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     </head>
     <body>
-<?php include "header.php" ?>
-
+        <?php
+        include "header.php";
+        include "config.php"
+        ?>
 
         <!--- content here --->
         <article class="container">
@@ -42,24 +44,21 @@
             </section>
 
             <article class="tab-content col-md-6">
+                <!--Guan Tsin's Part-->
                 <section id="profile" class="tab-pane fade in active">
                     <h2>Account Details</h2><hr class="line">
                     <form action="" method="post">
                         <p class="form-group">
                             <label for="account_first_name">First name&nbsp;<span class="required">*</span></label>
-                            <input type="text" required="true" class="form-control" name="account_first_name" id="account_first_name" autocomplete="given-name" value="sinnfei" />
+                            <input type="text" class="form-control" name="account_first_name" id="account_first_name" autocomplete="given-name" required/>
                         </p>
                         <p class="form-group">
                             <label for="account_last_name">Last name&nbsp;<span class="required">*</span></label>
-                            <input type="text" required="true" class="form-control" name="account_last_name" id="account_last_name" autocomplete="family-name" value="heng" />
-                        </p>
-                        <p class="form-group">
-                            <label for="account_display_name">Display name&nbsp;<span class="required">*</span></label>
-                            <input type="text" required="true" class="form-control" name="account_display_name" id="account_display_name" value="" />
+                            <input type="text" class="form-control" name="account_last_name" id="account_last_name" autocomplete="family-name" required/>
                         </p>
                         <p class="form-group">
                             <label for="account_email">Email address&nbsp;<span class="required">*</span></label>
-                            <input type="email" required="true" class="form-control" name="account_email" id="account_email" autocomplete="email" value="sinnfeiheng@gmail.com" />
+                            <input type="email" class="form-control" name="account_email" id="account_email" autocomplete="email" required/>
                         </p>
 
                         <fieldset>
@@ -82,66 +81,151 @@
                         <button type="submit" class="btn btn-primary" name="save_account_details" value="Save changes">Save changes</button>
                     </form>
                 </section>
-                <section id="paymentnshipping" class="tab-pane fade">
+                
+                <!--Sinn Fei's Part-->
+                <section id="paymentnshipping" name="pymtNshipping" class="tab-pane fade">
                     <h3>Credit/Debit Card</h3><hr class="line">
+                    <?php
+                    function maskedCardNum($ccnum) {
+                        $masked = str_pad(substr($ccnum, -4), strlen($ccnum), '*', STR_PAD_LEFT);
+                        return $masked;
+                    }
+
+                    $query = "SELECT address, postal_code FROM p1_8.user WHERE UID='$_SESSION[UID]'";
+                    $result = mysqli_query($db, $query);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        $row = mysqli_fetch_assoc($result);
+                        $addr = $row['address'];
+                        $postalcode = $row['postal_code'];
+                    }
+
+                    $query2 = "SELECT * FROM p1_8.payment WHERE UID='$_SESSION[UID]'";
+                    $result2 = mysqli_query($db, $query2);
+                    if (mysqli_num_rows($result2) > 0) {
+                        $row2 = mysqli_fetch_assoc($result2);
+                        $cardName = $row2['card_name'];
+                        $cardNum = $row2['card_number'];
+                        $expDate = $row2['card_expiry'];
+                        $cardtype = $row2['card_type'];
+
+                        echo "<section id='cardDetails'>";
+                        echo "<h5>Name on card: " . $cardName . "</h5>";
+                        echo "<h5>Card Type: " . $cardtype . "</h5>";
+                        echo "<h5>Card Number: " . maskedCardNum($cardNum) . "</h5>";
+                        echo "<h5>Expiry Date: " . $expDate . "</h5>";
+                        echo "</section>";
+                    } else {
+                        echo "<form name = 'myCardForm' action = 'process_card.php' method = 'post' onsubmit = 'return validateCardForm()'>";
+                        echo "<div class = 'form-group'>";
+                        echo "<label for = 'text'>Name on card*</label>";
+                        echo "<input class = 'form-control' type = 'text' id = 'cardName' name = 'cardName' placeholder = 'Full name on card' pattern = '^[a-zA-Z ]*$' required/>";
+                        echo "</div>";
+                        echo "<div class = 'form-group'>";
+                        echo "<label for = 'text'Card Type* </label>";
+                        echo "<span class = 'fa fa-cc-visa' style = 'color:navy;'></span>";
+                        echo "<span class = 'fa fa-cc-mastercard' style = 'color:black;'></span>";
+
+                        echo "<select class = 'form-control' name = 'cardtype' id = 'selectCards'>";
+                        echo "<option value = 'Visa'>Visa</option>";
+                        echo "<option value = 'Mastercard'>MasterCard</option>";
+                        echo "</select>";
+                        echo "</div>";
+                        echo "<script>
+                        var classes = {
+                            Visa: 'fa-cc-visa', Mastercard: 'fa-cc-mastercard'
+                        };
+
+                        $('#selectCards').on('change', function () {
+                        var value = $(this) . val();
+
+                        $('#cardIcon i') . attr('class', '');
+
+                        $('#cardIcon i') . addClass('fa' . addClass(classes[value]);
+                        })
+                        </script>";
+                        echo "<div class = 'form-group'>";
+                        echo "<label for = 'text'>Card Number*</label>";
+                        echo "<section class = 'input-group'>";
+                        echo "<input class = 'form-control' type = 'text' id = 'cardNum' name = 'cardNum' placeholder = 'Card Number' pattern = '4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11}' required/>";
+
+                        echo "<section class = 'input-group-addon'>";
+                        echo "<span id = 'cardIcon' class = 'icon'><i class = 'fa fa-cc-visa'></i></span>";
+                        echo "</section>";
+                        echo "</section>";
+                        echo "</div>";
+
+                        echo "<div class = 'row'>";
+                        echo "<div class = 'col-md-3'>";
+                        echo "<div class = 'form-group'>";
+                        echo "<label for = 'text'>Expiry Date*</label>";
+                        echo "<input class = 'form-control' type = 'text' id = 'expDate' name = 'expDate' placeholder = 'MM/YY' pattern = '(0[1-9]|1[012])[/]([2-9][0-9])' required />";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "<div class = 'col-md-3'";
+                        echo "<div class = 'form-group'>";
+                        echo "<label for = 'text'>CVV*</label>";
+                        echo "<input class = 'form-control' type = 'text' id = 'cvc' name = 'cvc' placeholder = 'CVV' pattern = '(?!000)[0-9]{3}' required/>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "<button type = 'submit' class = 'btn btn-primary'>Save</button>";
+                        echo "</form>";
+                    }
+                    ?>
                     <form>
-                        <p class="form-group">
-                            <label>Name on card</label>
-                            <input class="form-control" type="text" placeholder="Full name on card"/>
-                        </p>
 
-                        <section class="form-group">
-                            <label>Card Number</label>
-                            <section class="input-group">
-                                <input class="form-control" type="text" placeholder="Card Number"/>
-
-                                <section class="input-group-addon">
-                                    <i class="fa fa-cc-visa"></i>
-                                </section>
-                            </section>
-                        </section>
-
-                        <section class="row">
-                            <div class="col-md-3">
-                                <p class="form-group">
-                                    <label>Expiration</label>
-                                    <input class="form-control" type="text" placeholder="MM/YY"/>
-                                </p>
-                            </div>
-                            <div class="col-md-3">
-                                <p class="form-group">
-                                    <label>CV Code</label>
-                                    <input class="form-control" type="text" placeholder="CVC"/>
-                                </p>
-                            </div>
-                        </section>
-                        <button type="button" class="btn btn-primary">Add Card</button>
                     </form>
 
+
+                    <!-- Trigger the modal with a button -->
+                    <button type="button" <?php if ((mysqli_num_rows($result2) == 0)) { ?> style='display:none;'<?php } ?> id="ccDelBtn" class="btn btn-danger" data-toggle="modal" data-target="#myModal">Delete</button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Delete? </h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Are you sure you want to delete the card?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">No, Keep It</button>
+                                    <form action="process_deleteCard.php" method="post" >
+                                        <button type="submit" class="btn btn-danger" >Yes, Delete</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                     <h3>Shipping Address</h3><hr class="line">
-                    <form>
+                    <form name="myAddrForm" action="process_address.php" method="post" onsubmit="return validateAddrForm()">
                         <p class="form-group">
-                            <label>Street address 1</label>
-                            <input class="form-control" type="text" placeholder="Street address, P.O box"/>
+                            <label for="text">Street Address*</label>
+                            <?php echo "<input class='form-control' type='text' id='addr' name='addr' placeholder='Street address, P.O box, unit, floor' value='" . $addr . "' pattern='[A-Za-z0-9\-\(\)#@(\) ]+' required/>"; ?>
                         </p>
+
                         <p class="form-group">
-                            <label>Street address 2</label>
-                            <input class="form-control" type="text" placeholder="Apartment, unit, building, floor"/>
+                            <label for="text">Postal Code*</label>
+                            <?php echo "<input class='form-control' type='text' id='postalcode' name='postalcode' placeholder='######' value='" . $postalcode . "' pattern='[0-9]{5,6}' required/>"; ?>
                         </p>
-                        <p class="form-group">
-                            <label>Postal Code</label>
-                            <input class="form-control" type="text" placeholder="#####"/>
-                        </p>
-                        <button type="button" class="btn btn-primary">Add Address</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </form>
+                    <br>
+                    <p>* required</p>
                     </form>
                 </section>
             </article>
             <div class=col-md-3></div>
-
-        </article>
+        </article><!--
 
         <!--- footer--->
-<?php include "footer.php" ?>
+        <?php include "footer.php" ?>
         <!--- end footer--->
 
 
